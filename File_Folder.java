e645c4f6aa9fb781fb1398ef162a97aa3851f0b7
Mohammed Saqlain <120318579+Sq1754@users.noreply.github.com>
2023-02-14 22:06:27 +0530

    Update File_Folder.java

--- File_Folder.java
+++ File_Folder.java
@@ -9,13 +9,13 @@ import java.io.IOException;
 public class File_Folder {
 	public static void main (String[]args) {
 		File folder =  new File("C:\\Users\\Har Har MahaDev\\Desktop\\javafile");
-	    folder.mkdir();
+	    folder.mkdir();  // this method .mkdir will Create a folder
 	    for(int i = 2; i<=10;i++) {  // loop for making 9 files
 	    	File f1 = new File("C:\\Users\\Har Har MahaDev\\Desktop\\javafile\\"+"Table"+i+".txt");
 	    	try {
 				f1.createNewFile();
 				FileWriter f = new FileWriter(f1);
-				for(int j=1;j<=10;j++) {
+				for(int j=1;j<=10;j++) {  // this is for Multiplication Tables
 					String str = i+" x "+j+" = "+i*j+"\n";
 					f.append(str);
 				}
